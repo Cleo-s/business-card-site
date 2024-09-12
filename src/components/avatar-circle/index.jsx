17d22c68f97e27995ff81f7e4c355054875fe6a5
Cleo-s <cleo.mics@gmail.com>
2024-09-12 01:58:10 +0100

    add: info-card-circle, info-card-block fix: styling minor fixes

---
+++ src/components/avatar-circle/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './circle.scss';
+
+function AvatarCircle() {
+    return(
+        <div className='avatar-circle'>
+        </div>
+    )
+}
+
+export default AvatarCircle;
--- src/components/info-card/index.jsx
+++ src/components/info-card/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import OverlayImageblock from '../overlay-image-block';
 
 import './info-card.scss';
 
 function InfoCard() {
     return (
         <div className='info-card-div'>
-
+            <OverlayImageblock/>
         </div>
     )
 }
