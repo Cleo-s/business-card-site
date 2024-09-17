3ba5c8d23dde3ad3c52b46d94646891a73ac2ad7
Cleo-s <cleo.mics@gmail.com>
2024-09-18 00:22:29 +0100

    add: background images for each page, request form for patients
    fix: scss minor fixes

--- src/pages/contact-page/index.jsx
+++ src/pages/contact-page/index.jsx
@@ -1,14 +1,83 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
 
 import './contact-page.scss';
+
 import { Footer, Header } from '../../components/interface';
+import { Button } from '../../components/ui';
 
 function ContactMePage() {
+
+    const [shortRequestValue, setShortRequestValue] = useState('');
+    const [fullRequestValue, setFullRequestValue] = useState('');
+    const [userNameValue, setUserNameValue] = useState('');
+
+    const requestInfoObject = {
+        userName: userNameValue,
+        shortRequestInfo: shortRequestValue,
+        fullRequestInfo: fullRequestValue,
+    };
+    
+    const handleShortValueSave = (e) => {
+        
+        setShortRequestValue(e.target.value);
+    }
+    
+    const handleFullValueSave = (e) => {
+        
+        setFullRequestValue(e.target.value);
+    }
+
+    const handleNameValue = (e) => {
+
+        setUserNameValue(e.target.value);
+    }
+
+    console.log(requestInfoObject);
 
     return (
         <>
             <Header />
-            <h1 className='h1-contact'>Contact page</h1>
+            <main className='main-contact-page'>
+                <div className='input-request-div'>
+                    <p className='p-user-name-request'>
+                        Ваше Ім'я:
+                    </p>
+                    <input
+                        className='user-name-request'
+                        type='text'
+                        placeholder=''
+                        value={userNameValue}
+                        onChange={handleNameValue}
+                    />
+                    <p className='p-short-define-request'>
+                        Кратко опишіть ваше звернення:
+                    </p>
+                    <input
+                        className='short-define-request'
+                        type='text'
+                        placeholder=''
+                        value={shortRequestValue}
+                        onChange={handleShortValueSave}
+                    />
+                    <p className='p-full-define-request'>
+                        Детально опишіть що вас турбує:
+                    </p>
+                    <input
+                        className='full-define-request'
+                        type='text'
+                        placeholder=''
+                        value={fullRequestValue}
+                        onChange={handleFullValueSave}
+                    />
+
+                    <p>
+                        {shortRequestValue} {fullRequestValue}
+                    </p>
+                </div>
+
+                <Button className='send-request-button' />
+            </main>
             <Footer />
         </>
     )
